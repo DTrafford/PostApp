62319a2bb2183c005c2a6611bfc5e19391c92ff0
D Trafford <d.trafford@outlook.com>
2018-08-13 15:56:53 -0400

    fix

--- backend/app.js
+++ backend/app.js
@@ -35,7 +35,7 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use(express.static(__dirname + '/build'));
+app.use(express.static(__dirname + '/dist'));
 app.use(express.static(__dirname + '/dist/PostApp'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname + '/dist/PostApp/index.html'));
