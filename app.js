bf3bf8cf6dec9ac71cf5624966fa03b2937df5d7
D Trafford <d.trafford@outlook.com>
2018-08-14 08:25:24 -0400

    Fixing Paths

--- app.js
+++ app.js
@@ -23,18 +23,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use("/images", express.static(path.join('backend/images'))); // allows acces to the image folder
 
-// Allow CORS accress
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    res.setHeader(
-        'Access-Control-Allow-Methods',
-        'GET, POST, PATCH, PUT, DELETE, OPTIONS');
-    next();
-});
-
+// // Allow CORS accress
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader(
+//         'Access-Control-Allow-Headers',
+//         'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+//     res.setHeader(
+//         'Access-Control-Allow-Methods',
+//         'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+//     next();
+// });
+app.use("/images", express.static(path.join(__dirname, "/backend/images")));
 app.use(express.static(__dirname + '/build'));
 app.use(express.static(__dirname + '/dist/PostApp'));
 
