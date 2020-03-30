8c1949e951a5d7316708630477623514877f84e2
David Trafford <d.trafford@outlook.com>
2020-03-30 14:27:43 -0400

    clean up code

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,21 +1,19 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { Subscription } from 'rxjs';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { Subscription } from "rxjs";
 
-import { Post } from '../post.model';
-import { Reply } from '../reply.model';
-import { PostService } from '../post.service';
-import { PageEvent } from '@angular/material';
-import { AuthService } from '../../auth/auth.service';
+import { Post } from "../post.model";
+import { Reply } from "../reply.model";
+import { PostService } from "../post.service";
+import { PageEvent } from "@angular/material";
+import { AuthService } from "../../auth/auth.service";
 
 @Component({
-  selector: 'app-post-list',
-  templateUrl: './post-list.component.html',
-  styleUrls: ['./post-list.component.css']
+  selector: "app-post-list",
+  templateUrl: "./post-list.component.html",
+  styleUrls: ["./post-list.component.css"]
 })
-
 export class PostListComponent implements OnInit, OnDestroy {
-
   posts: Post[] = [];
   private postSub: Subscription;
   replyClicked = false;
@@ -36,33 +34,43 @@ export class PostListComponent implements OnInit, OnDestroy {
   private dNameListenerSubs: Subscription;
   private userIdListenerSubs: Subscription;
 
-  constructor(public postService: PostService, private authService: AuthService) { }
+  constructor(
+    public postService: PostService,
+    private authService: AuthService
+  ) {}
 
   ngOnInit() {
     this.isLoading = true;
     this.postService.getPosts(this.postsPerPage, this.currentPage);
 
-    this.postSub = this.postService.getPostUpdateListener()
-    .subscribe((postData: {posts: Post[], postCount: number}) => {
-      this.isLoading = false;
-      this.totalPosts = postData.postCount;
-      this.posts = postData.posts;
-    });
+    this.postSub = this.postService
+      .getPostUpdateListener()
+      .subscribe((postData: { posts: Post[]; postCount: number }) => {
+        this.isLoading = false;
+        this.totalPosts = postData.postCount;
+        this.posts = postData.posts;
+      });
 
     this.userAuthenticated = this.authService.getIsAuthenticated();
-    this.authListenerSubs = this.authService.getAuthStatusListener().subscribe(isAuthenticated => {
-      this.userAuthenticated = isAuthenticated;
-    });
+    this.authListenerSubs = this.authService
+      .getAuthStatusListener()
+      .subscribe(isAuthenticated => {
+        this.userAuthenticated = isAuthenticated;
+      });
 
     this.userId = this.authService.getUserId();
-    this.userIdListenerSubs = this.authService.getUserIdListener().subscribe(userId => {
-      this.userId = userId;
-    });
+    this.userIdListenerSubs = this.authService
+      .getUserIdListener()
+      .subscribe(userId => {
+        this.userId = userId;
+      });
 
     this.displayName = this.authService.getDisplayName();
-    this.dNameListenerSubs = this.authService.getDNameListener().subscribe(displayName => {
-      this.displayName = displayName;
-    });
+    this.dNameListenerSubs = this.authService
+      .getDNameListener()
+      .subscribe(displayName => {
+        this.displayName = displayName;
+      });
   }
 
   onReply(index: number) {
@@ -93,11 +101,14 @@ export class PostListComponent implements OnInit, OnDestroy {
 
   onDelete(postId: string) {
     this.isLoading = true;
-    this.postService.deletePost(postId).subscribe(() => {
-      this.postService.getPosts(this.postsPerPage, this.currentPage);
-    }, () => {
-      this.isLoading = false;
-    });
+    this.postService.deletePost(postId).subscribe(
+      () => {
+        this.postService.getPosts(this.postsPerPage, this.currentPage);
+      },
+      () => {
+        this.isLoading = false;
+      }
+    );
   }
 
   ngOnDestroy() {
@@ -107,22 +118,3 @@ export class PostListComponent implements OnInit, OnDestroy {
     this.userIdListenerSubs.unsubscribe();
   }
 }
-
-  // imageDisplayCheck(image: File | string ) {
-  //   if (this.fileTypeChecker(image)) {
-  //     this.fileTypeChecker(image);
-  //   } else {
-  //     return null;
-  //   }
-  // }
-  // fileTypeChecker(file: File | string) {
-  //   if (typeof(file) === 'object') {
-  //     return file.name.slice((Math.max(0, file.name.lastIndexOf('.')) || Infinity) + 1);
-  //   } else {
-  //     if (file.slice((Math.max(0, file.lastIndexOf('.')) || Infinity) + 1)) {
-  //       return file.slice((Math.max(0, file.lastIndexOf('.')) || Infinity) + 1);
-  //     } else {
-  //       return null;
-  //     }
-  //   }
-  // }
--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -1,125 +1,136 @@
-import { Post } from './post.model';
-import { Reply } from './reply.model';
-import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
-import { Router } from '@angular/router';
-import { environment } from '../../environments/environment';
+import { Post } from "./post.model";
+import { Reply } from "./reply.model";
+import { Injectable } from "@angular/core";
+import { Subject } from "rxjs";
+import { HttpClient } from "@angular/common/http";
+import { map } from "rxjs/operators";
+import { Router } from "@angular/router";
+import { environment } from "../../environments/environment";
 
-const BACKEND_URL = environment.apiUrl + '/posts';
+const BACKEND_URL = environment.apiUrl + "/posts";
 
 @Injectable()
 export class PostService {
+  constructor(private http: HttpClient, private router: Router) {}
+  posts: Post[] = [];
+  private postsUpdated = new Subject<{ posts: Post[]; postCount: number }>();
+  replyArray: Reply[] = [];
 
-    constructor(private http: HttpClient, private router: Router) {}
-    posts: Post[] = [];
-    private postsUpdated = new Subject<{posts: Post[], postCount: number}>();
-    replyArray: Reply[] = [];
-
-    getPosts(postsPerPage: number, currentPage: number) {
-      const queryParams = `?pagesize=${postsPerPage}&page=${currentPage}`;
-        this.http
-            .get<{ message: string, posts: any, totalPosts: number }>(
-              BACKEND_URL + queryParams)
-            .pipe(map((postData) => {
-                return { posts: postData.posts.map(post => {
-                    return {
-                        id: post._id,
-                        title: post.title,
-                        content: post.content,
-                        creatorId: post.creatorId,
-                        creatorName: post.creatorName,
-                        replies: post.replies,
-                        imagePath: post.imagePath
-                    };
-                }), totalPosts: postData.totalPosts};
-            }))
-            .subscribe((returnedPostsData) => {
-              this.posts = returnedPostsData.posts;
-              this.postsUpdated.next({posts: [...this.posts], postCount: returnedPostsData.totalPosts});
-            });
-    }
-
-    getPostUpdateListener() {
-        return this.postsUpdated.asObservable();
-    }
-
-    getPost(id: string) {
-      return this.http.get<Post>(BACKEND_URL + '/' + id);
-    }
-
-    addPost(post: Post, image: File | string) {
-      let postData: Post | FormData;
-      if (image) {
-      postData = new FormData();
-      postData.append('title', post.title);
-      postData.append('content', post.content);
-      postData.append('replies', JSON.stringify(post.replies)); // Because replies is an array
-      postData.append('image', image, post.title);
-      } else {
-        postData = {
-          id: post.id,
-          title: post.title,
-          content: post.content,
-          creatorId: post.creatorId,
-          creatorName: null,
-          replies: post.replies,
-          imagePath: null
-        };
-      }
-      this.http.post<{message: string, post: Post}>(BACKEND_URL, postData)
-        .subscribe((responseData) => {
-          this.router.navigate(['/']);
-        });
-    }
-
-    updatePost(id: string, post: Post, image: string) {
-      // Nessecary for dynamic file updates and must have image: File | string as a paramater
-      // let postData: Post | FormData;
-      // if (typeof image === 'object') {
-      //   postData = new FormData();
-      //   postData.append('id', id);
-      //   postData.append('title', post.title);
-      //   postData.append('content', post.content);
-      //   postData.append('replies', JSON.stringify(post.replies));
-      //   postData.append('image', image);
-      // } else {
-
-      let postData: Post;
-          postData = {
-            id: post.id,
-            title: post.title,
-            content: post.content,
-            creatorId: post.creatorId,
-            creatorName: null,
-            replies: post.replies,
-            imagePath: image
+  getPosts(postsPerPage: number, currentPage: number) {
+    const queryParams = `?pagesize=${postsPerPage}&page=${currentPage}`;
+    this.http
+      .get<{ message: string; posts: any; totalPosts: number }>(
+        BACKEND_URL + queryParams
+      )
+      .pipe(
+        map(postData => {
+          return {
+            posts: postData.posts.map(post => {
+              return {
+                id: post._id,
+                title: post.title,
+                content: post.content,
+                creatorId: post.creatorId,
+                creatorName: post.creatorName,
+                replies: post.replies,
+                imagePath: post.imagePath
+              };
+            }),
+            totalPosts: postData.totalPosts
           };
-        // }
-        this.http.put(BACKEND_URL + '/' + id, postData)
-        .subscribe((response) => {
-          this.router.navigate(['/']);
+        })
+      )
+      .subscribe(returnedPostsData => {
+        this.posts = returnedPostsData.posts;
+        this.postsUpdated.next({
+          posts: [...this.posts],
+          postCount: returnedPostsData.totalPosts
         });
-    }
+      });
+  }
 
-    addReply(id: string, reply: Reply, post: Post) {
-      if (post.replies) {
-        post.replies.push(reply);
-      } else {
-        post.replies = [
-          {
-            user: reply.user,
-            replyMessage: reply.replyMessage
-          }];
-      }
-      this.http.put(BACKEND_URL + '/' + id + '/addReply', post)
-      .subscribe((responseData) => {
+  getPostUpdateListener() {
+    return this.postsUpdated.asObservable();
+  }
+
+  getPost(id: string) {
+    return this.http.get<Post>(BACKEND_URL + "/" + id);
+  }
+
+  addPost(post: Post, image: File | string) {
+    let postData: Post | FormData;
+    if (image) {
+      postData = new FormData();
+      postData.append("title", post.title);
+      postData.append("content", post.content);
+      postData.append("replies", JSON.stringify(post.replies)); // Because replies is an array
+      postData.append("image", image, post.title);
+    } else {
+      postData = {
+        id: post.id,
+        title: post.title,
+        content: post.content,
+        creatorId: post.creatorId,
+        creatorName: null,
+        replies: post.replies,
+        imagePath: null
+      };
+    }
+    this.http
+      .post<{ message: string; post: Post }>(BACKEND_URL, postData)
+      .subscribe(responseData => {
+        this.router.navigate(["/"]);
+      });
+  }
+
+  updatePost(id: string, post: Post, image: string) {
+    /******************************* */
+    // Nessecary for dynamic file updates and must have image: File | string as a paramater
+    // let postData: Post | FormData;
+    // if (typeof image === 'object') {
+    //   postData = new FormData();
+    //   postData.append('id', id);
+    //   postData.append('title', post.title);
+    //   postData.append('content', post.content);
+    //   postData.append('replies', JSON.stringify(post.replies));
+    //   postData.append('image', image);
+    // } else {
+    /******************************** */
+    let postData: Post;
+    postData = {
+      id: post.id,
+      title: post.title,
+      content: post.content,
+      creatorId: post.creatorId,
+      creatorName: null,
+      replies: post.replies,
+      imagePath: image
+    };
+    // }
+    this.http.put(BACKEND_URL + "/" + id, postData).subscribe(response => {
+      this.router.navigate(["/"]);
+    });
+  }
+
+  addReply(id: string, reply: Reply, post: Post) {
+    if (post.replies) {
+      post.replies.push(reply);
+    } else {
+      post.replies = [
+        {
+          user: reply.user,
+          replyMessage: reply.replyMessage
+        }
+      ];
+    }
+    this.http
+      .put(BACKEND_URL + "/" + id + "/addReply", post)
+      .subscribe(responseData => {
         console.log(responseData);
       });
-    }
+  }
 
-    deletePost(postId: string) {
-        return this.http.delete(BACKEND_URL + '/' + postId);
-    }
+  deletePost(postId: string) {
+    return this.http.delete(BACKEND_URL + "/" + postId);
+  }
 }
